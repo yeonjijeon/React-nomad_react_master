122b19a586cdc0fe887605d3e41f03f485eba9af
YeonJi <jeonyeonji1028@gmail.com>
2023-09-18 21:38:10 +0900

    selector

--- src/App.js
+++ src/App.js
@@ -20,12 +20,23 @@ const Box = styled.div`
   width: 200px;
   background-color: tomato;
   animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+  }
 `
 
 function App() {
   return (
     <Wrapper>
-      <Box></Box>
+      <Box>
+        <span>😀</span>
+      </Box>
     </Wrapper>
   )
 }
